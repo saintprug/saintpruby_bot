54a1cabec19c1fd315716b81647bce78ef9bc2d1
Vitaly Pecherin <readfuzza@gmail.com>
2019-04-28 18:51:44 +0300

    Improvement: use TELEGRAM_BOT_API_TOKEN instead TOKEN env name

--- runner.rb
+++ runner.rb
@@ -6,7 +6,7 @@ Dotenv.load
 
 logger = Logger.new(STDOUT)
 
-Telegram::Bot::Client.run(ENV['TOKEN'], logger: logger) do |bot|
+Telegram::Bot::Client.run(ENV['TELEGRAM_BOT_API_TOKEN'], logger: logger) do |bot|
   dispatcher = Dispatcher.new(bot)
 
   bot.listen do |message|
